3b1f86782ab551816c4b718457dc9fa6144440d4
Maritza Gonzalez <59942843+mcgonzale@users.noreply.github.com>
2021-02-21 21:46:52 -0500

    Create UserReviews.java
    Create UserReviews
    Text reviews and numeral scores
    Eventually, this data will need to be fastened to code that stores data in text file/database.

---
+++ app/src/main/java/UserReviews.java
@@ -0,0 +1,80 @@
+//MCG 2-21-2021
+import java.io.File;    //Eventually we are going to want to save this info to a text file I believe
+
+public class UserReviews {
+
+    //Written Review Inner Class
+    private class WrittenReview {
+        private String writtenReview;
+
+        private String getWrittenReview() {
+            return writtenReview;
+        }
+
+        private int getCharacterCount() {    //Return length of written review minus number of spaces.
+            int spaceCount = 0;
+            for (char ch : writtenReview.toCharArray()) {
+                if (ch == ' ') {
+                    spaceCount++;
+                }
+            }
+            return writtenReview.length() - spaceCount;
+        }
+
+        /*
+        TODO
+        stretch goal word count method per review method
+         */
+
+        private void setWrittenReview(String writtenReview) {
+            this.writtenReview = writtenReview;
+        }
+
+        public WrittenReview() {
+            this.writtenReview = "";
+        }
+
+        public WrittenReview(String review) {
+            this.writtenReview = review;
+        }
+    }
+    //-----------------------------------------------------------------
+
+
+    //Instance variables
+    private WrittenReview wr;
+    private int numeralScore = 0;
+
+
+    //Constructors
+    public UserReviews(){
+        wr = new WrittenReview();
+    }
+    public UserReviews(String r){
+        wr = new WrittenReview(r);
+    }
+    public UserReviews(int score){
+        wr = new WrittenReview();
+        numeralScore = score;
+    }
+    public UserReviews(String r, int score){
+        wr = new WrittenReview(r);
+        numeralScore = score;
+    }
+
+
+    //Getters and setters
+    public int getNumeralScore() {
+        return numeralScore;
+    }
+    public void setNumeralScore(int numeralScore) {
+        this.numeralScore = numeralScore;
+    }
+    public String getWrittenReview(){
+        return wr.getWrittenReview();
+    }
+    public void setWrittenReview(String review) {
+        this.wr.setWrittenReview(review);
+    }
+
+}
