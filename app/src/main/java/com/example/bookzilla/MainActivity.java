78d421906631687c92454f22f3a7baa3eb88ba23
SidRegmiS <ssregmi@mtu.edu>
2021-04-04 15:17:34 -0400

    wishlist tested and wishlist image set up

--- app/src/main/java/com/example/bookzilla/MainActivity.java
+++ app/src/main/java/com/example/bookzilla/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bookzilla;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,16 +52,15 @@ class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedList
 
 
 public class MainActivity extends AppCompatActivity {
-
-    /* Instance Variables */
     ArrayList<String> wishlist = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SpinnerActivity spinnerActivity = new SpinnerActivity();
+
+        // Hello world :)
         
 /*-------------------Start Code for dropdownBox-----------*/
         Spinner spinner = findViewById(R.id.status);
@@ -78,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
     /*-------------end for the code of dropdownBox---------------*/
-        UpdateCurrentUserTextView();
     }
 
     // TODO code for the button that navigates to the BookViewActivity -- delete when another way of navigation has been developed
@@ -120,16 +122,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void UpdateCurrentUserTextView() {
-        TextView userProfileText = (TextView) findViewById(R.id.currentUserTextView);
-        if (CurrentUserProfile.profile == null) {
-            userProfileText.setText("CURRENT USER: NULL");
+   /* public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
 
-        }
-        else {
-            userProfileText.setText("CURRENT USER: " + CurrentUserProfile.profile.getName());
-        }
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
-
+    */
 
 }
--- app/src/main/java/com/example/bookzilla/WishlistActivity.java
+++ app/src/main/java/com/example/bookzilla/WishlistActivity.java
@@ -1,5 +1,8 @@
 package com.example.bookzilla;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,15 +11,19 @@ import java.util.ArrayList;
 public class WishlistActivity extends AppCompatActivity {
 
     ArrayList<Book> books = new ArrayList<>();
+    ArrayList<String> booksString = new ArrayList<>();
 
+    private ListView listView;
+    private UserProfile currentPerson;
 
-    public Book removeBook(Book book){
+    public void removeBook(Book book){
         books.remove(book);
-        return book;
+        booksString.remove(book.getTitle());
     }
 
     public void addBook(Book book){
         books.add(book);
+        booksString.add(book.getTitle());
     }
 
 
@@ -25,5 +32,14 @@ public class WishlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wishlist);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_wishlist, booksString);
+
+        //listView = (ListView) findViewById(R.id.imageView);
+        //listView.setAdapter(adapter);
+
     }
+
+
+
 }
